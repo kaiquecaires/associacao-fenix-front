900e779f4e47b081b19bedcae40aae8549afca40
kaiquecaires <kaique.caires@yahoo.com>
2021-03-05 14:53:42 -0300

    feat: add header to signup page

--- src/external-interfaces/pages/signup/index.tsx
+++ src/external-interfaces/pages/signup/index.tsx
@@ -1,10 +1,14 @@
+import Image from 'next/image'
 import sharedStyles from '../../shared-styles/styles.module.css'
 import styles from './styles.module.css'
+import Logo from '../../../../public/logo.svg'
 
 const SignUp: React.FC = () => {
   return (
     <div className={`${sharedStyles.primaryColor} ${styles.container}`}>
-      <header className={styles.header}>teste</header>
+      <header className={styles.header}>
+        <Logo />
+      </header>
     </div>
   )
 }
