f14a59f051f7d0f4913f95aaf792b7ac412eef80
kaiquecaires <kaique.caires@yahoo.com>
2021-03-05 13:41:54 -0300

    feat: add assets

--- src/external-interfaces/pages/signup/index.tsx
+++ src/external-interfaces/pages/signup/index.tsx
@@ -1,8 +1,11 @@
 import sharedStyles from '../../shared-styles/styles.module.css'
+import styles from './styles.module.css'
 
 const SignUp: React.FC = () => {
   return (
-    <div className={sharedStyles.primaryColor}>Hello world</div>
+    <div className={`${sharedStyles.primaryColor} ${styles.container}`}>
+      <header className={styles.header}>teste</header>
+    </div>
   )
 }
 export default SignUp
